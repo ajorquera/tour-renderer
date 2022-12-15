f8441503949e0d1045ac0597134332a39b2d3e6a
andres <jorquera.ad@gmail.com>
2022-12-15 16:01:23 -0400

    fixing

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,11 +1,24 @@
 import { defineConfig } from "rollup";
 import dts from 'rollup-plugin-dts'
 import esbuild from 'rollup-plugin-esbuild'
+import resolve from '@rollup/plugin-node-resolve'
+import alias from '@rollup/plugin-alias';
 
 export default defineConfig([
     {
         input: `src/index.ts`,
-        plugins: [esbuild()],
+        plugins: [esbuild(), 
+          
+          alias({
+            entries: [
+              { find: 'react', replacement: 'preact/compat' },
+              { find: 'react-dom/test-utils', replacement: 'preact/test-utils' },
+              { find: 'react-dom', replacement: 'preact/compat' },
+              { find: 'react/jsx-runtime', replacement: 'preact/jsx-runtime' }
+            ]
+          }),
+          
+          resolve()],
         output: [
           {
             file: `dist/TourRenderer.js`,
